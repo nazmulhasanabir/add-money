c9d2d9baba7ba5df92a854d439be96f105b728bd
nazmulhasanabir <nazmulhasanabir2024@gmail.com>
2024-09-19 16:34:52 +0600

    added money button

--- jss/home.js
+++ jss/home.js
@@ -1,1 +1,10 @@
-console.log('confirm')
+document.getElementById('add-money-Btn').addEventListener('click',function(event){
+event.preventDefault();
+
+const  phoneNumber = document.getElementById('phone-number').value;
+console.log(phoneNumber)
+
+
+const   pincode = document.getElementById('pinNumber').value;
+console.log(pincode);
+});
